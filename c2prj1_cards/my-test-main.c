742b2b3cd26cbc4c27771b7cf63f06c3a74399ff
Coursera Learner <nobody@nowhere.nul>
2021-03-18 18:30:51 +0000

    test case

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,24 @@
 #include "cards.h"
+#include <stdio.h>
 
 int main(void) {
+  // Test assert
+  card_t card = {12, SPADES};
+  //assert_card_valid(card);
 
+  // Test raking_to_string
+  hand_ranking_t ranking = STRAIGHT_FLUSH;
+  printf("%s\n", ranking_to_string(ranking));
+
+  hand_ranking_t ranking1 = 111;
+  printf("%s\n", ranking_to_string(ranking1));
+
+  // Test print_card
+  print_card(card);
+
+  // Test card from letter
+  card_t card_1 = card_from_letters('J', 's');
+  print_card(card_1);
+  
+  return 0;
 }
