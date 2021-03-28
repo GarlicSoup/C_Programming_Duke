9959ee7b3fccae93c248981ac0f38a6ae645ebab
Coursera Learner <nobody@nowhere.nul>
2021-03-28 03:20:21 +0000

    a29

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -4,5 +4,10 @@
 #include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+
+  char name_extension[] = ".count";
+  char * output_file_name = malloc((strlen(inputName)+strlen(name_extension)+1) * sizeof(output_file_name));
+  strcpy(output_file_name, inputName);
+  strcat(output_file_name, name_extension);
+  return output_file_name;
 }
