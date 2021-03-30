854130bffaa5bb8cced5c6833660531ab3244eb6
Coursera Learner <nobody@nowhere.nul>
2021-03-30 19:24:20 +0000

    fixed bugs

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -6,45 +6,79 @@
 #include <ctype.h>
 
 deck_t * hand_from_string(const char * str, future_cards_t * fc) {
-  deck_t *cur_deck = NULL;
-  cur_deck = malloc(sizeof(*cur_deck));
-  cur_deck->n_cards = 0;
-  cur_deck->cards = NULL;
-  char *p=strchr(str,'\n');
-  if(p!=NULL){
-    *p='\0';
-  }
-  // printf("%s\n", str);
-  // printf("hand string length %zu\n", strlen(str));
-  for (size_t i=0; i<strlen(str)+1; i++) {
-    if (str[i+1] == '\0' || str[i] == '\0') {
-      break;
-    }
-    if (str[i+1] == ' ' || str[i] == ' ') {
-      continue;
-    }
+  /* deck_t *cur_deck = NULL; */
+  /* cur_deck = malloc(sizeof(*cur_deck)); */
+  /* cur_deck->n_cards = 0; */
+  /* cur_deck->cards = NULL; */
+  /* char *p=strchr(str,'\n'); */
+  /* if(p!=NULL){ */
+  /*   *p='\0'; */
+  /* } */
+  /* // printf("%s\n", str); */
+  /* // printf("hand string length %zu\n", strlen(str)); */
+  /* for (size_t i=0; i<strlen(str)+1; i++) { */
+  /*   if (str[i+1] == '\0' || str[i] == '\0') { */
+  /*     break; */
+  /*   } */
+  /*   if (str[i+1] == ' ' || str[i] == ' ') { */
+  /*     continue; */
+  /*   } */
       
-    if (str[i] != '?') {
-      card_t card = card_from_letters(str[i], str[i+1]);
-      add_card_to(cur_deck, card);
-      //print_card(card);
-      //printf("\n");
+  /*   if (str[i] != '?') { */
+  /*     card_t card = card_from_letters(str[i], str[i+1]); */
+  /*     add_card_to(cur_deck, card); */
+  /*     //print_card(card); */
+  /*     //printf("\n"); */
+  /*   } */
+  /*   else if (str[i] == '?' && isdigit(str[i+1])) { */
+  /*     //printf("Should not have this!\n"); */
+  /*     card_t * empty_card = add_empty_card(cur_deck); */
+  /*     print_card(*empty_card); */
+  /*     printf("\n"); */
+  /*     add_future_card(fc, str[i+1], empty_card); */
+  /*   } */
+  /*   else { */
+  /*     fprintf(stderr,"Error: Invalid input file format, line %s",str); */
+  /*     card_from_letters(0,0); */
+  /*   } */
+  /* } */
+  /* // print_hand(cur_deck); */
+  
+  /* return cur_deck; */
+  deck_t *ans=malloc(sizeof(*ans));
+  assert(ans!=NULL);
+  ans->cards=NULL;
+  ans->n_cards=0;
+  size_t i=0;
+  while(str[i]!='\0'){
+    if(str[i]=='?' && isdigit(str[i+1])){
+      char *number=malloc(2*sizeof(*number));
+      assert(number!=NULL);
+      size_t j=0;
+      number[j++]=str[i+1];
+      number[j]='\0';
+      if(isdigit(str[i+2])){
+	number=realloc(number,3*sizeof(*number));
+	assert(number!=NULL);
+	number[j++]=str[i+2];
+	number[j]='\0';
+	i++;
+      }
+      add_future_card(fc,atoi(number),add_empty_card(ans));
+      free(number);
     }
-    else if (str[i] == '?' && isdigit(str[i+1])) {
-      //printf("Should not have this!\n");
-      card_t * empty_card = add_empty_card(cur_deck);
-      print_card(*empty_card);
-      printf("\n");
-      add_future_card(fc, str[i+1], empty_card);
+    else if((isdigit(str[i]) || str[i]=='A' || str[i]=='K' || str[i]=='Q' || str[i]=='J') && (str[i+1]=='s' || str[i+1]=='h' || str[i+1]=='d' || str[i+1]=='c')){
+      add_card_to(ans,card_from_letters(str[i],str[i+1]));
     }
-    else {
+    else{
       fprintf(stderr,"Error: Invalid input file format, line %s",str);
       card_from_letters(0,0);
     }
-  }
-  // print_hand(cur_deck);
-  
-  return cur_deck;
+    i++;
+    while(isspace(str[i+1]) && str[i+1]!='\0'){
+      i++;
+    }
+    return ans;
 }
 
 deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc) {
