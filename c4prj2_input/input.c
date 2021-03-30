764b1a574f7ac9279765d906f5aa94985b1524a7
Coursera Learner <nobody@nowhere.nul>
2021-03-30 19:42:25 +0000

    fixed bugs

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -49,102 +49,64 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc) {
     }
   }
   return cur_deck;
-
-  
-  /* deck_t *ans=malloc(sizeof(*ans)); */
-  /* assert(ans!=NULL); */
-  /* ans->cards=NULL; */
-  /* ans->n_cards=0; */
-  /* size_t i=0; */
-  /* while(str[i]!='\0'){ */
-  /*   if(str[i]=='?' && isdigit(str[i+1])){ */
-  /*     char *number=malloc(2*sizeof(*number)); */
-  /*     assert(number!=NULL); */
-  /*     size_t j=0; */
-  /*     number[j++]=str[i+1]; */
-  /*     number[j]='\0'; */
-  /*     if(isdigit(str[i+2])){ */
-  /* 	number=realloc(number,3*sizeof(*number)); */
-  /* 	assert(number!=NULL); */
-  /* 	number[j++]=str[i+2]; */
-  /* 	number[j]='\0'; */
-  /* 	i++; */
-  /*     } */
-  /*     add_future_card(fc,atoi(number),add_empty_card(ans)); */
-  /*     free(number); */
-  /*   } */
-  /*   else if((isdigit(str[i]) || str[i]=='A' || str[i]=='K' || str[i]=='Q' || str[i]=='J') && (str[i+1]=='s' || str[i+1]=='h' || str[i+1]=='d' || str[i+1]=='c')){ */
-  /*     add_card_to(ans,card_from_letters(str[i],str[i+1])); */
-  /*   } */
-  /*   else{ */
-  /*     fprintf(stderr,"Error: Invalid input file format, line %s",str); */
-  /*     card_from_letters(0,0); */
-  /*   } */
-  /*   i++; */
-  /*   while(isspace(str[i+1]) && str[i+1]!='\0'){ */
-  /*     i++; */
-  /*   } */
-  /*   i++; */
-  /* } */
-  /* return ans; */
 }
 
 deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc) {
-  /* char * line = NULL; */
-  /* size_t sz = 0; */
-  /* deck_t ** deck = malloc(sizeof(*deck)); */
-  /* if (f == NULL) { */
-  /*   perror("Error: Could not read file"); */
-  /*   exit(EXIT_FAILURE); */
-  /* } */
-  /* while (getline(&line, &sz, f) >= 0) { */
-  /*   (*n_hands)++; */
-  /*   deck = realloc(deck, (*n_hands)* sizeof(*deck)); */
-  /*   assert(deck != NULL); */
-  /*   deck_t * cur_deck = hand_from_string(line, fc); */
-  /*   deck[*n_hands-1] = cur_deck; */
-  /*   //print_hand(cur_deck); */
-  /*   if(deck[(*n_hands)-1]->n_cards<5){ */
-  /*     fprintf(stderr,"Error: Invalid number of cards in input file\n"); */
-  /*     exit(EXIT_FAILURE); */
-  /*   } */
-  /*   free(line); */
-  /*   line=NULL; */
-  /* } */
-  /* free(line); */
-
-  /* if (fclose(f) != 0) { */
-  /*   perror("Error: Could not close file!\n"); */
-  /*   exit(EXIT_FAILURE); */
-  /* } */
-  /* return deck; */
-  
-  deck_t **ans=NULL;
-  /**n_hands=0;*/
-
-  char *line=NULL;
-  size_t sz=0;
-  while(getline(&line,&sz,f)>=0){
-    char *p=strchr(line,'\n');
-    if(p!=NULL){
-      *p='\0';
-    }
-    else{
-      fprintf(stderr,"Error: Invalid line '%s' in input file!\n",line);
-      assert(p!=NULL);
-    }
-    ans=realloc(ans,((*n_hands)+1)*sizeof(*ans));
-    assert(ans!=NULL);
+  char * line = NULL;
+  size_t sz = 0;
+  deck_t ** deck = malloc(sizeof(*deck));
+  if (f == NULL) {
+    perror("Error: Could not read file");
+    exit(EXIT_FAILURE);
+  }
+  while (getline(&line, &sz, f) >= 0) {
     (*n_hands)++;
-    ans[(*n_hands)-1]=hand_from_string(line,fc);
-    if(ans[(*n_hands)-1]->n_cards<5){
+    deck = realloc(deck, (*n_hands)* sizeof(*deck));
+    assert(deck != NULL);
+    deck_t * cur_deck = hand_from_string(line, fc);
+    deck[*n_hands-1] = cur_deck;
+    //print_hand(cur_deck);
+    if(deck[(*n_hands)-1]->n_cards<5){
       fprintf(stderr,"Error: Invalid number of cards in input file\n");
-      assert(ans[(*n_hands)-1]->n_cards>=5);
+      exit(EXIT_FAILURE);
     }
     free(line);
-    sz=0;
+    line=NULL;
   }
   free(line);
 
-  return ans;
+  if (fclose(f) != 0) {
+    perror("Error: Could not close file!\n");
+    exit(EXIT_FAILURE);
+  }
+  return deck;
+  
+  /* deck_t **ans=NULL; */
+  /* /\**n_hands=0;*\/ */
+
+  /* char *line=NULL; */
+  /* size_t sz=0; */
+  /* while(getline(&line,&sz,f)>=0){ */
+  /*   char *p=strchr(line,'\n'); */
+  /*   if(p!=NULL){ */
+  /*     *p='\0'; */
+  /*   } */
+  /*   else{ */
+  /*     fprintf(stderr,"Error: Invalid line '%s' in input file!\n",line); */
+  /*     assert(p!=NULL); */
+  /*   } */
+  /*   ans=realloc(ans,((*n_hands)+1)*sizeof(*ans)); */
+  /*   assert(ans!=NULL); */
+  /*   (*n_hands)++; */
+  /*   ans[(*n_hands)-1]=hand_from_string(line,fc); */
+  /*   if(ans[(*n_hands)-1]->n_cards<5){ */
+  /*     fprintf(stderr,"Error: Invalid number of cards in input file\n"); */
+  /*     assert(ans[(*n_hands)-1]->n_cards>=5); */
+  /*   } */
+  /*   free(line); */
+  /*   sz=0; */
+  /* } */
+  /* free(line); */
+
+  /* return ans; */
 }
